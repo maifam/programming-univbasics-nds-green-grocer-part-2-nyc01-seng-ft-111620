0f3888f5f10e05ec28b5ab7426d6f313ad0c348b
Mai Pham <maitphm@gmail.com>
2020-11-13 20:11:53 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,9 +1,8 @@
 require_relative './part_1_solution.rb'
 
 def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  
+  new_price
 end
 
 def apply_clearance(cart)
