17b7bda3231d1457ef862570dafb4b8f760aaacd
Mai Pham <maitphm@gmail.com>
2020-11-13 20:34:03 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -30,9 +30,10 @@ end
 
 
 def apply_clearance(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  
+  counter = 0 
+
+
 end
 
 def checkout(cart, coupons)
