472798597199ba356654a082d3ceb3870c2f4a57
Mai Pham <maitphm@gmail.com>
2020-11-13 20:35:14 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -32,6 +32,9 @@ end
 def apply_clearance(cart)
   
   counter = 0 
+  while counter < cart.length 
+    if cart[counter][:clearance]
+      cart[counter][:price] = (cart[counter][:price] - (cart[counter][:price] *))
 
 
 end
