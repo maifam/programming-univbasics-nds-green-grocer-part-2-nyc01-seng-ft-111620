7c24642c792109c142a8830c4f3f6c14a359a859
Mai Pham <maitphm@gmail.com>
2020-11-13 20:22:44 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -11,6 +11,8 @@ def apply_coupons(cart, coupons)
       if cart_item_with_coupon 
         cart_item_with_coupon[:count] += coupons[counter][:num]
         cart_item[:count] -= coupons[counter][:num]
+      else 
+        cart_item_with_coupon = { 
   
 end
 
