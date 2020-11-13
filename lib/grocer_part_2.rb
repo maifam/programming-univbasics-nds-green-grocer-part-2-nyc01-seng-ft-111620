1012dc74434537758da184f1fad828dd3b02966a
Mai Pham <maitphm@gmail.com>
2020-11-13 20:16:35 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -4,7 +4,7 @@ def apply_coupons(cart, coupons)
   
   counter = 0 
   while counter < cart.length 
-    cart_item = find_item_by_name_in_collection
+    cart_item = find_item_by_name_in_collection(coupons[counter][:item], cart)]
   
 end
 
